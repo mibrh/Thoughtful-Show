0f7529f29cc5bb17c77cda5af162f81cca26a925
Mohammad Ibrahim <moham64@outlook.com>
2018-02-25 14:07:26 -0500

    Set up recyclerview onclicklistener

--- app/src/main/java/com/mibrh/thoughtfulshow/Controllers/MainActivity.java
+++ app/src/main/java/com/mibrh/thoughtfulshow/Controllers/MainActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.mibrh.thoughtfulshow.App;
 import com.mibrh.thoughtfulshow.Clients.YoutubeClient;
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // setContentView now to avoid crash
         setContentView(R.layout.activity_main);
 
         App.setContext(getApplicationContext());
@@ -71,5 +82,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         Log.d(TAG, "YoutubeClient.getList called and video list set");
+
+        // RecyclerView OnItemClickListener
+        recyclerViewVideos.addOnItemTouchListener(new RecyclerTouchListener(App.getContext(), recyclerViewVideos, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Video video = videoList.get(position);
+                Toast.makeText(getApplicationContext(), video.getTitle() + " clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Video video = videoList.get(position);
+                Toast.makeText(getApplicationContext(), video.getTitle() + " long clicked", Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
+
+
 }
