4484685e97130c492567009bc1cff1409740ee47
Mohammad Ibrahim <moham64@outlook.com>
2018-02-27 18:49:44 -0500

    changed restclient timeout back to 10000

--- app/src/main/java/com/mibrh/thoughtfulshow/Clients/RESTClient.java
+++ app/src/main/java/com/mibrh/thoughtfulshow/Clients/RESTClient.java
@@ -8,7 +8,7 @@ import com.loopj.android.http.RequestParams;
 public class RESTClient {
     private static String TAG = "RESTClient";
     private static AsyncHttpClient client = new AsyncHttpClient();
-    private static final int TIME_OUT = 40000;
+    private static final int TIME_OUT = 10000;
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.setTimeout(TIME_OUT);
