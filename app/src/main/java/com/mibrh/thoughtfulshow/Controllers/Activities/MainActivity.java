9bc4f1cf766cae128f93893100601fe49d4af36a
Mohammad Ibrahim <moham64@outlook.com>
2018-02-27 19:12:02 -0500

    back to full screen

--- app/src/main/java/com/mibrh/thoughtfulshow/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/mibrh/thoughtfulshow/Controllers/Activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.mibrh.thoughtfulshow.Controllers.Activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.mibrh.thoughtfulshow.App;
 import com.mibrh.thoughtfulshow.Controllers.Fragments.VideoListFragment;
@@ -17,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "MainActivity onCreate");
         super.onCreate(savedInstanceState);
-//        // Remove title bar
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        // Remove notification bar
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // setContentView now to avoid crash
         setContentView(R.layout.activity_main);
         App.setContext(getApplicationContext());
